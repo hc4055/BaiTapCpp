e71a299cff9fb0bf687c4c010cef67b16eb7e37a
hc4055 <ultimatewar555@gmail.com>
2021-02-26 15:55:27 +0700

    Tuan 1

---
+++ T1_B21_DTTamGiac.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std ;
+
+int main()
+{
+	double a, b, c, s, area ;
+	cin >> a >> b >> c;
+	if(a+b>c && b+c>a && a+c>b) 
+	{
+	s = ( a + b + c ) / 2 ;
+	area = sqrt ( s* (s-a) * (s-b) * (s-c) ) ;
+	cout << area ;
+	} 
+	return 0;
+}	
