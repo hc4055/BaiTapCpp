b3f74cb5cd0f8c2bd788cbc3fffd7f6b0d20223d
hc4055 <ultimatewar555@gmail.com>
2021-02-26 15:44:36 +0700

    Tuan 1

--- T1_B15_Fibonacci.cpp
+++ T1_B15_Fibonacci.cpp
@@ -8,13 +8,13 @@ int main()
 	
 	string f0 = "a" , f1 = "b" , f2 = f0 + f1 ;
 	
-	cout << f0 << endl << f1 << endl << f2 << endl;
+	cout << "f0: " << f0 << endl << "f1: " << f1 << endl << "f2: " << f2 << endl ;
 	
-	for ( i = 0; i < 10 ; i++ )
+	for ( i = 0; i < 8 ; i++ )
 		{
 			f0 = f1;
 			f1 = f2;
 			f2 = f1 + f0;
-			cout << "f" << i+1 << ":" << f2 << endl;
+			cout << "f" << i+3 << ": " << f2 << endl ;
 		}
 }	
