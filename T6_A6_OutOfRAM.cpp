6d013068c9f9103e1265341204378b171bfd9d80
hc4055 <ultimatewar555@gmail.com>
2021-03-26 07:45:56 +0700

    T6

---
+++ T6_A6_OutOfRAM.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void recursive(int n)
+{
+	int i;
+    int f[100000];
+    i++;
+    cout << i << endl;
+    recursive(n);
+}
+int main()
+{
+    int i = 0;
+    int n;
+    cin >> n;
+    recursive(n);
+    return 0;
+}
